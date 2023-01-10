666737c20a3e5a571d7255595b914df70d8080e1
Tatyana <lioness1741@mail.ru>
2023-01-10 21:39:59 +0500

    Первое задание готово, доработка.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,34 +1,46 @@
-"use strict";
-let a;
-let b;
-let c;
-function solveEquation(a, b, c) {
+'use strict';
+let aValue;
+let bValue;
+let cValue;
+
+function solveEquation(aValue, bValue, cValue) {
+ if (aValue == 0) {
+    return false;
+  }
   let arr = [];
-  a = 2;
-  b = 6;
-  c = 1;
-  let discriminant = b * b - 4 * a * c;
-  console.log(discriminant);
-  if (discriminant < 0) {
-    console.log(arr);
-  }  else if (discriminant == 0) {
-    let squareRoot = -b / (2 * a);
-    arr.push(squareRoot);
-    console.log(arr);
-  }  else if (discriminant > 0) {
-    let squareRoot1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let squareRoot2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  aValue = 2;
+  bValue = 6;
+  cValue = 2;
+  let discriminant = bValue * bValue - 4 * aValue * cValue;
+  console.log('Дискриминант = ' + discriminant);
+
+  if (discriminant > 0) {
+    let squareRoot1 = (-bValue + Math.sqrt(discriminant)) / (2 * aValue);
+    let squareRoot2 = (-bValue - Math.sqrt(discriminant)) / (2 * aValue);
+
     arr.push(squareRoot1);
-    arr.push(squareRoot2);   
+    arr.push(squareRoot2);
+    console.log('Дискриминант > 0');
+    console.log(arr);
+
+  } else if (discriminant == 0) {
+    let squareRoot = -bValue / (2 * aValue);
+    arr.push(squareRoot);
+    console.log('Дискриминант = 0');
+    console.log(arr);
+  } else {
+    return false;
+    console.log('Дискриминант < 0');
     console.log(arr);
   }
   return arr;
 }
-  solveEquation(a, b, c);
+solveEquation(aValue, bValue, cValue);
 
 
 
-console.log("-------------------------------------");
+
+console.log("---------------------------------");
 
 
 let percent = 10;
@@ -36,29 +48,43 @@ let contribution = 0;
 let amount = 50000;
 let countMonths = 12;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
-  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
-    let theEnd = "Неверно введено значение. Проверьте данные!"; 
-    console.log(theEnd);
-  }  else {
-    let contiNue = "Посчитаем.";  
-    console.log(contiNue);
+if (isNaN(percent)) {
+    let checkOut1 = 'Неверно введено значение процентов по кредиту. Проверьте данные!';
+    console.log(checkOut1);
+  } else if (isNaN(contribution)) {
+    let checkOut2 = 'Неверно введено значение первоначального взноса по кредиту. Проверьте данные!'; 
+    сonsole.log(checkOut2);
+  }  else if (isNaN(amount)) {
+    let checkOut3 = 'Неверно введено значение суммы кредита. Проверьте данные!';
+    console.log(checkOut3);
+  } else if (isNaN(countMonths)) {
+    let checkOut4 = 'Неверно введено количество месяцев по кредиту. Проверьте данные!';
+    console.log(checkOut4);
+  } else {
+    let letsCount = 'Посчитаем.';
+    console.log(letsCount);
   }
 
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
   let percentPerMonth = percent / 100 / 12;
-  console.log(percentPerMonth);
+  console.log(percentPerMonth + ' процентов за пользование кредитом в месяц!');
+  
   let amountBody = amount - contribution;
   let root = Math.pow((1 + percentPerMonth), countMonths);
-  console.log(root);
+  
   let paymentPerMonth = amountBody * (percentPerMonth + (percentPerMonth / (root - 1)));
-  console.log(paymentPerMonth);   
+  console.log(amountBody.toFixed(2) + ' рублей за погашение основного долга по кредиту!')
+  console.log(paymentPerMonth.toFixed(2) + ' рублей платеж по кредиту за месяц!');
+  
   let summAll = paymentPerMonth * countMonths;
-  console.log(summAll);
-  console.log(summAll.toFixed(2));
+  console.log(summAll.toFixed(2) + ' рублей общая сумма платежа по кредиту!');
+  
   if (isNaN(summAll)) {
     console.log("Проверьте корректность введенных данных!");
-  }  else {
-  return summAll;
+  } else {
+    return summAll;
   }
 }
+
 calculateTotalMortgage(percent, contribution, amount, countMonths);
