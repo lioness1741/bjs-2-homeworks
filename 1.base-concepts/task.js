6913807558cbfe05569ed4391af37ec0f36cd6c8
Tatyana <lioness1741@mail.ru>
2023-01-23 22:55:24 +0500

    Первое задание доработка 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,23 @@
 'use strict';
-let aValue;
-let bValue;
-let cValue;
 
 function solveEquation(aValue, bValue, cValue) {
- if (aValue == 0) {
-    return false;
+  if (isNaN(aValue) || isNaN(bValue) || isNaN(cValue)) {
+  console.log('Данные введены неверно. Проверьте!');
+    return;
+  }
+ 
+
+  if (aValue == 0) {
+    return;
   }
   let arr = [];
-  aValue = 2;
-  bValue = 6;
-  cValue = 2;
+
   let discriminant = bValue * bValue - 4 * aValue * cValue;
   console.log('Дискриминант = ' + discriminant);
-
+ 
   if (discriminant > 0) {
     let squareRoot1 = (-bValue + Math.sqrt(discriminant)) / (2 * aValue);
     let squareRoot2 = (-bValue - Math.sqrt(discriminant)) / (2 * aValue);
-
     arr.push(squareRoot1);
     arr.push(squareRoot2);
     console.log('Дискриминант > 0');
@@ -29,62 +29,62 @@ function solveEquation(aValue, bValue, cValue) {
     console.log('Дискриминант = 0');
     console.log(arr);
   } else {
-    return false;
     console.log('Дискриминант < 0');
     console.log(arr);
   }
   return arr;
 }
-solveEquation(aValue, bValue, cValue);
+solveEquation(2, 4, 8);
 
 
 
 
-console.log("---------------------------------");
 
-
-let percent = 10;
-let contribution = 0;
-let amount = 50000;
-let countMonths = 12;
-
-if (isNaN(percent)) {
-    let checkOut1 = 'Неверно введено значение процентов по кредиту. Проверьте данные!';
-    console.log(checkOut1);
-  } else if (isNaN(contribution)) {
-    let checkOut2 = 'Неверно введено значение первоначального взноса по кредиту. Проверьте данные!'; 
-    сonsole.log(checkOut2);
-  }  else if (isNaN(amount)) {
-    let checkOut3 = 'Неверно введено значение суммы кредита. Проверьте данные!';
-    console.log(checkOut3);
-  } else if (isNaN(countMonths)) {
-    let checkOut4 = 'Неверно введено количество месяцев по кредиту. Проверьте данные!';
-    console.log(checkOut4);
-  } else {
-    let letsCount = 'Посчитаем.';
-    console.log(letsCount);
-  }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
+if (isNaN(percent)) {
+  let checkOut1 = 'Неверно введено значение процентов по кредиту. Проверьте данные!';
+  console.log(checkOut1);
+  return;
+}
+else if (isNaN(contribution)) {
+  let checkOut2 = 'Неверно введено значение первоначального взноса по кредиту. Проверьте данные!';
+  сonsole.log(checkOut2);
+  return;
+}
+else if (isNaN(amount)) {
+  let checkOut3 = 'Неверно введено значение суммы кредита. Проверьте данные!';
+  console.log(checkOut3);
+  return;
+}
+else if (isNaN(countMonths)) {
+  let checkOut4 = 'Неверно введено количество месяцев по кредиту. Проверьте данные!';
+  console.log(checkOut4);
+  return;
+}
+else {
+  let letsCount = 'Посчитаем.';
+  console.log(letsCount);
+}
+
+
+  
   let percentPerMonth = percent / 100 / 12;
   console.log(percentPerMonth + ' процентов за пользование кредитом в месяц!');
-  
+
   let amountBody = amount - contribution;
   let root = Math.pow((1 + percentPerMonth), countMonths);
-  
+
   let paymentPerMonth = amountBody * (percentPerMonth + (percentPerMonth / (root - 1)));
   console.log(amountBody.toFixed(2) + ' рублей за погашение основного долга по кредиту!')
   console.log(paymentPerMonth.toFixed(2) + ' рублей платеж по кредиту за месяц!');
-  
+
   let summAll = paymentPerMonth * countMonths;
   console.log(summAll.toFixed(2) + ' рублей общая сумма платежа по кредиту!');
-  
-  if (isNaN(summAll)) {
-    console.log("Проверьте корректность введенных данных!");
-  } else {
-    return summAll;
-  }
+
+  if (isNaN(summAll));
+  return;
 }
 
-calculateTotalMortgage(percent, contribution, amount, countMonths);
+calculateTotalMortgage(10, 0, 50000, 12);
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -13,63 +13,42 @@ function getArrayParams(...arr) {
   let finalAnswer = { min: min, max: max, avg: avg };
   console.log(finalAnswer);
   return finalAnswer;
-
 }
 
 getArrayParams(-99, 99, 10);
 
 
-console.log('-----------------------------------------------')
-console.log('-----------------------------------------------')
+
+
 
 
 function summElementsWorker(...arr) {
 
-  let hasOneNumber = arr.some(arr => {
-    return arr;
-  })
-  if (hasOneNumber == false) {
+  if (arr.length === 0) {
     return 0;
   }
-  let result = arr.reduce((sum, current) => sum + current, 0);
-  return result;
+  return (arr.reduce((sum, current) => sum + current, 0));
 }
 
 console.log(summElementsWorker(10, 10, 11, 20, 10));
 
-console.log('-----------------------------------------------')
 
 function differenceMaxMinWorker(...arr) {
 
-  let hasOneNumber = arr.some(arr => {
-    return arr;
-  })
-  if (hasOneNumber == false) {
+  if (arr.length === 0) {
     return 0;
   }
-
-  let min;
-  let max;
-  max = Math.max.apply(null, arr);
-  min = Math.min.apply(null, arr);
-  let difference = max - min;
-
-  return difference;
+  return (Math.max.apply(null, arr) - Math.min.apply(null, arr));
 }
 
 console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
 
-console.log('-----------------------------------------------')
 
 function differenceEvenOddWorker(...arr) {
 
-  let hasOneNumber = arr.some(arr => {
-    return arr;
-  })
-  if (hasOneNumber == false) {
+  if (arr.length === 0) {
     return 0;
   }
-
   let sumEvenElement = 0;
   let sumOddElement = 0;
 
@@ -80,20 +59,15 @@ function differenceEvenOddWorker(...arr) {
       sumOddElement += arr[i];
     }
   }
-  let difference = sumEvenElement - sumOddElement;
-  return difference;
+  return (sumEvenElement - sumOddElement);
 }
 
 console.log(differenceEvenOddWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-console.log('-----------------------------------------------')
 
 function averageEvenElementsWorker(...arr) {
 
-  let hasOneNumber = arr.some(arr => {
-    return arr;
-  })
-  if (hasOneNumber == false) {
+  if (arr.length === 0) {
     return 0;
   }
   let sumEvenElement = 0;
@@ -103,20 +77,14 @@ function averageEvenElementsWorker(...arr) {
     if (arr[i] % 2 == 0) {
       sumEvenElement += arr[i];
       countEvenElement += 1;
-
-    } else {
-      continue;
     }
   }
-
-  let divisionMiddle = sumEvenElement / countEvenElement;
-  return divisionMiddle;
+  return (sumEvenElement / countEvenElement);
 }
 console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
 
 
-console.log('-----------------------------------------------')
 
 
 
@@ -126,30 +94,17 @@ const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [3
 function makeWork(arrOfArr, func) {
 
   let maxWorkerResult = -Infinity;
-  let resArrOfArr;
-  let arrMax = [];
-  console.log('----- ------- ------ ------- -----')
-  for (let i = 0; i < arr.length; i++) {
-    arrOfArr = arr[i];
-    resArrOfArr = [];
-    for (let j = 0; j < arrOfArr.length; j++) {
-      resArrOfArr.push(arrOfArr[j]);
-      differenceEvenOddWorker(...arr[i]);
-    }
-    arrMax.push(differenceEvenOddWorker(...arr[i]));
-  }
 
-  for (let i = 0; i < arrMax.length; i++) {
-    if (arrMax[i] >= maxWorkerResult) {
-      maxWorkerResult = arrMax[i];
-    } else {
-      continue;
-    }
-  }
-  console.log(maxWorkerResult);
-  console.log('++++++++++++++++++')
-  return resArrOfArr;
+ 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const res = func(...arrOfArr[i]);
 
+    if (res > maxWorkerResult) {
+      maxWorkerResult = res;
+    }
+    console.log(maxWorkerResult);
+  }
+  return maxWorkerResult;
 }
 
-makeWork([[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]], summElementsWorker);
+makeWork([[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]], averageEvenElementsWorker);
